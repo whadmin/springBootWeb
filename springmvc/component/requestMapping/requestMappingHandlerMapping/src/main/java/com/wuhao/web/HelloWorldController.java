59c4c0b4cedc4799ee10b1ff5d26befe3dde60c8
wuhao.w <806033113@qq.com>
2020-06-11 17:45:27 +0800

    commit

--- springmvc/component/requestMapping/requestMappingHandlerMapping/src/main/java/com/wuhao/web/HelloWorldController.java
+++ springmvc/component/requestMapping/requestMappingHandlerMapping/src/main/java/com/wuhao/web/HelloWorldController.java
@@ -4,16 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractUrlViewController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * HelloWorld {@link Controller}
  */
 @Controller
-public class HelloWorldController {
+public class HelloWorldController extends AbstractUrlViewController {
 
     @ResponseBody
     @RequestMapping("/hello")
     public String index(Model model) {
         return "hello";
+    }
+
+
+    @Override
+    protected String getViewNameForRequest(HttpServletRequest request) {
+        return null;
     }
 }
